0f7bfd49461739c24f48b319e0c98ad4ad6f116c
slipper <samuel.lippert@ifactr.com>
2015-09-29 10:43:04 -0500

    Initial code commit.

---
+++ AssemblyInfoRevisionLabeller/AssemblyInfoRevisionLabeller.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Exortech.NetReflector;
+using ThoughtWorks.CruiseControl.Core;
+
+namespace CCNet.AssemblyInfoRevisionLabeller.Plugin
+{
+    [ReflectorType("assemblyInfoRevisionLabeller")]
+    public class AssemblyInfoRevisionLabeller : ILabeller
+    {
+        public string Generate(IIntegrationResult previousResult)
+        {
+            Regex regex = new Regex(Pattern);
+
+            string content = string.Join(Environment.NewLine, File.ReadAllLines(Path).Where(l => !l.Trim().StartsWith("//")).ToArray());
+            string revision = Exec("git", "rev-list --count HEAD", WorkingDirectory);
+            bool svn = false;
+            if (string.IsNullOrEmpty(revision))
+            {
+                revision = Exec("svn", "info --show-item revision", WorkingDirectory);
+                svn = true;
+            }
+
+            var versionMatch = regex.Match(content).Groups["version"].Value.TrimEnd('.', '*');
+            Version version;
+            try
+            {
+                version = new Version(versionMatch);
+            }
+            catch (Exception)
+            {
+                throw new Exception($"Could not read version format for <{Path}>: {versionMatch}");
+            }
+
+            int revNum;
+            if (!int.TryParse(revision, out revNum))
+            {
+                throw new Exception($"Could not read revision from {(svn ? "svn" : "git")} working copy <{WorkingDirectory}>: {revision}");
+            }
+
+            return new Version(Math.Max(1, version.Major),
+                               Math.Max(0, version.Minor),
+                               Math.Max(0, version.Build),
+                               revNum)
+                           .ToString();
+        }
+
+        public void Run(IIntegrationResult result)
+        {
+            result.Label = Generate(result);
+        }
+
+        [ReflectorProperty("path", Required = true)]
+        public string Path { get; set; }
+
+        [ReflectorProperty("workingDirectory", Required = true)]
+        public string WorkingDirectory { get; set; }
+
+        [ReflectorProperty("pattern", Required = false)]
+        public string Pattern { get; set; } = @"AssemblyVersion\(""(?<version>[^\""]*)""\)";
+
+        private static string Exec(string command, string args, string dir)
+        {
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = command,
+                    Arguments = args,
+                    CreateNoWindow = true,
+                    WorkingDirectory = dir,
+                }
+            };
+            p.Start();
+
+            var output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return output.Trim();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AssemblyInfoRevisionLabeller
 A CruiseControl.NET "labeller" plugin that combines an AssemblyVersion with a git count or svn revision.
+
+Based on code from http://predicatet.blogspot.com/2007/10/version-custom-cruisecontrolnet.html
